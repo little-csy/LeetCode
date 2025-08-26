c55295f00b5591a1bb999b2d8ded9175ba2a7aa2
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-08-25 19:07:03 -0700

    LC-swap-nodes-in-pairs [Runtime: 0 ms; Memory: 17.7 MB]

---
+++ linked-list/swap-nodes-in-pairs.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode()
+        dummyHead.next = head
+        cur = dummyHead
+
+        while cur.next is not None and cur.next.next is not None:
+            temp1 = cur.next
+            temp2 = cur.next.next.next
+            cur.next = cur.next.next
+            cur.next.next = temp1
+            temp1.next = temp2
+            cur = temp1
+
+        return dummyHead.next
+
+        
