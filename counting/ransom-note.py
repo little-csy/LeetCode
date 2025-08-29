c033e55af5122b07e88be693893e0152fd877e8e
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-08-28 18:39:29 -0700

    LC-ransom-note [Runtime: 15 ms; Memory: 18.1 MB]

---
+++ counting/ransom-note.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        r = Counter(ransomNote)
+        m = Counter(magazine)
+        if r&m == r:
+            return True
+        else:
+            return False
