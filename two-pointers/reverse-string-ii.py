ca809fe333c524d968b3730d23fce3ce6d4e3cde
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-08-30 13:58:31 -0700

    LC-reverse-string-ii [Runtime: 3 ms; Memory: 17.8 MB]

--- two-pointers/reverse-string-ii.py
+++ two-pointers/reverse-string-ii.py
@@ -2,9 +2,6 @@ class Solution:
     def reverseStr(self, s: str, k: int) -> str:
         s = list(s)
         for i in range(0, len(s), 2*k):
-            if i+k <= len(s):
                 s[i:i+k] = s[i:i+k][::-1]
-                continue
-            s[i:] = s[i:][::-1]
         
         return "".join(s)
