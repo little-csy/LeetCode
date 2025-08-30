c5ed16e9f7eda3b509d2e92b7298c7c1072bddfc
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-08-30 12:44:41 -0700

    LC-reverse-string [Runtime: 3 ms; Memory: 23.1 MB]

--- string/reverse-string.py
+++ string/reverse-string.py
@@ -3,5 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
-        
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+            left += 1
+            right -= 1
