7b1eed4e9565b08360e89006bddfdd21a4f6673d
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-01 17:59:26 -0700

    LC-repeated-substring-pattern [Runtime: 47 ms; Memory: 18.2 MB]

--- string-matching/repeated-substring-pattern.py
+++ string-matching/repeated-substring-pattern.py
@@ -1,9 +1,19 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
-        ss = s+s
-        cutss = ss[1:len(ss)-1]
-
-        if cutss.find(s) == -1:
-            return False
-        else:
-            return True
+        next = [0] * len(s)
+        self.getNext(s, next)
+        if next[-1]!=0:
+            repeat = len(s)-next[-1]
+            if len(s) % repeat == 0:
+                return True
+        return False
+        
+    def getNext(self, s:str, next:List[int]) -> None:
+        j = 0
+        next[0] = 0
+        for i in range(1, len(s)):
+            while j>0 and s[i]!=s[j]:
+                j = next[j-1]
+            if s[i] == s[j]:
+                j +=1
+            next[i] = j
