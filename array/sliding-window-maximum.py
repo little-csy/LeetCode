ef589c0588903781c8c064f4297219f3651787f8
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-14 17:45:48 -0700

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

--- array/sliding-window-maximum.py
+++ array/sliding-window-maximum.py
@@ -1,15 +1,10 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         result = []
-        if len(nums) == 1:
-            return [nums[0]]
-        elif len(nums) == 2:
-            if k > 1:
-                return [max(nums[0], nums[1])]
-            else:
-                return [nums[0], nums[1]]
+        if k == 1:
+            return nums
         else:
-            for i in range(len(nums)-2):
+            for i in range(len(nums)-k+1):
                 window_max = nums[i]
                 for j in range(i+1, i+k):
                     if nums[j] > window_max:
