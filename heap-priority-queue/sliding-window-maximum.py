b9dd8eb827905b4795ceba84d4ac1d2e5d044e48
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-14 17:39:11 -0700

    LC-sliding-window-maximum [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/sliding-window-maximum.py
+++ heap-priority-queue/sliding-window-maximum.py
@@ -4,14 +4,14 @@ class Solution:
         if len(nums) == 1:
             return [nums[0]]
         elif len(nums) == 2:
-            return max(nums[0], nums[1])
+            return [max(nums[0], nums[1])]
         else:
             for i in range(len(nums)-2):
-                max = nums[i]
+                window_max = nums[i]
                 for j in range(i+1, i+k):
-                    if nums[j] > max:
-                        max = nums[j]
-                result.append(max)
+                    if nums[j] > window_max:
+                        window_max = nums[j]
+                result.append(window_max)
             
             return result
         
