312706ebbdef29f06fb1368b58eb5b5ed15f0e16
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-14 23:55:29 -0700

    LC-top-k-frequent-elements [Runtime: 7 ms; Memory: 21.2 MB]

--- bucket-sort/top-k-frequent-elements.py
+++ bucket-sort/top-k-frequent-elements.py
@@ -1,14 +1,19 @@
+import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         map = defaultdict(int)
-        result = []
+        result = [0] * k
+        prior_que = []
 
         for num in nums:
             map[num] += 1
         
-        sorted_items = sorted(map.items(), key = lambda x:x[1], reverse = True)
-        for i in range(k):
-            result.append(sorted_items[i][0])
-    
-        return result
+        for key,value in map.items():
+            heapq.heappush(prior_que, (value,key))
+            if len(prior_que) > k:
+                heapq.heappop(prior_que)
         
+        for i in range(k-1, -1, -1):
+            result[i] = heapq.heappop(prior_que)[1]
+        
+        return result
