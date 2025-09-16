a15b1c14d66e51dd0e05ec2382fc4f9b64afed81
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-16 15:37:37 -0700

    LC-binary-tree-postorder-traversal [Runtime: 0 ms; Memory: 17.8 MB]

--- binary-tree/binary-tree-postorder-traversal.py
+++ binary-tree/binary-tree-postorder-traversal.py
@@ -6,14 +6,17 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        self.res = []
-        self.postTrav(root)
-        return self.res
-    
-    def postTrav(self, cur:Optional[TreeNode]):
-        if cur is None:
-            return
-        
-        self.postTrav(cur.left)
-        self.postTrav(cur.right)
-        self.res.append(cur.val)     
+        stack = []
+        res = []
+        stack.append(root)
+
+        while stack:
+            node = stack.pop()
+            if node is None:
+                continue
+            
+            res.append(node.val)
+            stack.append(node.left)
+            stack.append(node.right)
+
+        return res[::-1]
