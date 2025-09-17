40c0b9117e85fc89a3f6037361c78a9caa32db38
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-16 18:58:32 -0700

    LC-linked-list-cycle-ii [Runtime: 41 ms; Memory: 19.6 MB]

---
+++ two-pointers/linked-list-cycle-ii.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                meet = fast
+                index = head
+                while meet != index:
+                    meet = meet.next
+                    index = index.next
+                return index
+            
+        return None
