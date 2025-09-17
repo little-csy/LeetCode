9cc705465843a0c25134f0ed48d905fb1f86889d
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-17 16:05:56 -0700

    LC-count-complete-tree-nodes [Runtime: 3 ms; Memory: 23.4 MB]

---
+++ tree/count-complete-tree-nodes.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        right = self.countNodes(root.right)
+        left = self.countNodes(root.left)
+        return right+left+1
