0a4106f10d680184f93da4dfa8e21e51af054df1
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-16 20:00:30 -0700

    LC-populating-next-right-pointers-in-each-node [Runtime: 51 ms; Memory: 19.2 MB]

---
+++ tree/populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root is None:
+            return
+        que = deque()
+        que.append(root)
+        root.next = None
+
+        while que:
+            size = len(que)
+            pre = None
+            while size:
+                node = que.popleft()
+                if pre:
+                    pre.next = node
+                pre = node
+                if node.left:
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
+                size -= 1
+            pre.next = None
+        
+        return root
