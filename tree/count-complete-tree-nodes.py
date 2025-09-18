f495100bd06eed98e13f450c37890355e112eb5e
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-17 19:05:21 -0700

    LC-count-complete-tree-nodes [Runtime: 0 ms; Memory: 23.3 MB]

--- tree/count-complete-tree-nodes.py
+++ tree/count-complete-tree-nodes.py
@@ -8,6 +8,18 @@ class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
-        right = self.countNodes(root.right)
+        left = root.left
+        right = root.right
+        leftnum = 0
+        rightnum = 0
+        while left:
+            left = left.left
+            leftnum += 1
+        while right:
+            right = right.right
+            rightnum += 1
+        if leftnum == rightnum:
+            return 2 ** (leftnum+1) - 1
         left = self.countNodes(root.left)
-        return right+left+1
+        right = self.countNodes(root.right)
+        return left+right+1
