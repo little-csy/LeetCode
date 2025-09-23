af890369952aff21a1c2fef32db190ce3042dd20
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-23 16:53:15 -0700

    LC-median-of-two-sorted-arrays [Runtime: 0 ms; Memory: 17.9 MB]

---
+++ divide-and-conquer/median-of-two-sorted-arrays.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums1 = nums1 + nums2
+        nums1.sort()
+        len1 = len(nums1)
+        mid = len1//2
+        if len1 % 2 == 1:
+            return nums1[mid]
+        if len1 % 2 == 0:
+            return (nums1[mid]+nums1[mid-1])/2
+        
