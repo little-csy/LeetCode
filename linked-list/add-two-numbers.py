d6f9c469e54e7912b4ed288c4743a87580e04cfa
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-23 15:12:51 -0700

    LC-add-two-numbers [Runtime: N/A; Memory: N/A]

---
+++ linked-list/add-two-numbers.py
@@ -0,0 +1,62 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        p1 = l1
+        p2 = l2
+        head = ListNode()
+        p3 = head
+        flag = 0
+        while p1 or p2:
+            if p1 is None:
+                if flag == 0:
+                    val = p2.val
+                    flag = 0
+                else:
+                    if p2.val+1>=10:
+                        val = p2.val-9
+                        flag = 1
+                    else:
+                        val = p2.val
+                        flag = 0
+                p2 = p2.next
+            if p2 is None:
+                if flag == 0:
+                    val = p1.val
+                    flag = 0
+                else:
+                    if p1.val+1>=10:
+                        val = p1.val-9
+                        flag = 1
+                    else:
+                        val = p1.val
+                        flag = 0
+                p1 = p1.next
+            if p1 and p2:
+                if p1.val+p2.val >= 10:
+                    if flag == 0:
+                        val = p1.val+p2.val-10
+                        flag = 1
+                    else:
+                        val = p1.val+p2.val-9
+                        flag = 1
+                else:
+                    if flag == 0:
+                        val = p1.val+p2.val
+                        flag = 0
+                    else:
+                        val = p1.val+p2.val+1
+                        flag = 0
+                p1 = p1.next
+                p2 = p2.next
+            new = ListNode(val, None)
+            p3.next = new
+            p3 = new
+        if flag == 1:
+            new = ListNode(1, None)
+            p3.next = new
+        return head.next
+        
