1e461adb0cc683590a654b515b43c72a3b4c7607
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-29 17:39:06 -0700

    LC-intersection-of-two-linked-lists [Runtime: N/A; Memory: N/A]

---
+++ hash-table/intersection-of-two-linked-lists.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        if headA is None or headB is None:
+            return None
+        p1 = headA
+        c1 = 1
+        p2 = headB
+        c2 = 1
+        while p1:
+            p1 = p1.next
+            c1 += 1
+        while p2:
+            p2 = p2.next
+            c2 += 1
+        
+        if c1 > c2:
+            d = c1 - c2
+            p1 = headA
+            p2 = headB
+            while d:
+                p1 = p1.next
+                d -= 1
+            while p1 and p2:
+                if p1 == p2:
+                    return p1
+                p1 = p1.next
+                p2 = p2.next
+            return None
+        
+        if c2 > c1:
+            d = c2 - c1
+            p1 = headA
+            p2 = headB
+            while d:
+                p2 = p2.next
+                d -= 1
+            while p1 and p2:
+                if p1 == p2:
+                    return p1
+                p1 = p1.next
+                p2 = p2.next
+            return None
+
+
+        
