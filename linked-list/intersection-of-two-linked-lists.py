6d772819c3e6d1b6e002a695c8983273be7beedd
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-29 17:49:27 -0700

    LC-intersection-of-two-linked-lists [Runtime: 91 ms; Memory: 27.8 MB]

--- linked-list/intersection-of-two-linked-lists.py
+++ linked-list/intersection-of-two-linked-lists.py
@@ -9,43 +9,18 @@ class Solution:
         if headA is None or headB is None:
             return None
         p1 = headA
-        c1 = 0
         p2 = headB
-        c2 = 0
-        while p1:
-            p1 = p1.next
-            c1 += 1
-        while p2:
-            p2 = p2.next
-            c2 += 1
+        while p1 != p2:
+            if p1:
+                p1 = p1.next
+            else:
+                p1 = headB
+            if p2:
+                p2 = p2.next
+            else:
+                p2 = headA
         
-        if c1 > c2:
-            d = c1 - c2
-            p1 = headA
-            p2 = headB
-            while d:
-                p1 = p1.next
-                d -= 1
-            while p1 and p2:
-                if p1 == p2:
-                    return p1
-                p1 = p1.next
-                p2 = p2.next
-            return None
-        
-        else:
-            d = c2 - c1
-            p1 = headA
-            p2 = headB
-            while d:
-                p2 = p2.next
-                d -= 1
-            while p1 and p2:
-                if p1 == p2:
-                    return p1
-                p1 = p1.next
-                p2 = p2.next
-            return None
+        return p1
 
 
         
