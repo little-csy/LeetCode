b991171bfcc6e5b61f741616aee832aebf601faa
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-09-29 17:29:06 -0700

    LC-sort-list [Runtime: 172 ms; Memory: 33 MB]

--- sorting/sort-list.py
+++ sorting/sort-list.py
@@ -5,19 +5,38 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        p = head
-        arr = []
-        while p:
-            arr.append(p.val)
-            p = p.next
-        
-        arr.sort()
+        if head is None or head.next is None:
+            return head
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        return self.merge(left, right)
+
+    def merge(self, l1:Optional[ListNode], l2:Optional[ListNode]):
         dummy = ListNode(0)
-        p = dummy
-        for v in arr:
-            q = ListNode(v)
-            p.next = q
-            p = p.next
-        
+        cur = dummy
+        p1 = l1
+        p2 = l2
+        while p1 and p2:
+            if p1.val < p2.val:
+                cur.next = p1
+                p1 = p1.next
+            else:
+                cur.next = p2
+                p2 = p2.next
+            cur = cur.next
+        if p1:
+            cur.next = p1
+        if p2:
+            cur.next = p2
         return dummy.next
+
