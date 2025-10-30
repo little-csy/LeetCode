d78050c0ae7cb739eb066c33ba955732e03de965
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-10-29 19:57:31 -0700

    LC-diameter-of-binary-tree [Runtime: 3 ms; Memory: 20.7 MB]

--- depth-first-search/diameter-of-binary-tree.py
+++ depth-first-search/diameter-of-binary-tree.py
@@ -6,14 +6,15 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        left = self.dfs(root.left)
-        right = self.dfs(root.right)
-        return left+right
+        self.ans = 0
+        self.dfs(root)
+        return self.ans
     
     def dfs(self, root: Optional[TreeNode]) -> int:
         if root is None:
             return 0
         left = self.dfs(root.left)
         right = self.dfs(root.right)
+        self.ans = max(self.ans, left + right)
         return max(left, right)+1
         
