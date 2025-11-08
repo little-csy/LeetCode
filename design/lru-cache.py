7c6053a000a2f8a625ce565b60ec539b049f538e
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-08 10:33:56 -0800

    LC-lru-cache [Runtime: 148 ms; Memory: 77.9 MB]

--- design/lru-cache.py
+++ design/lru-cache.py
@@ -1,54 +1,52 @@
 class Node:
-    def __init__(self, key:int, value:int):
+    def __init__(self, key:int, val:int):
         self.key = key
-        self.value = value
+        self.val = val
         self.prev = None
         self.next = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
         self.head.next = self.tail
         self.tail.prev = self.head
+        self.mp = {}
         self.capacity = capacity
-        self.hash = {}
-    
-    def addn(self, node:Node):
-        node.next = self.tail
-        node.prev = self.tail.prev
-        self.tail.prev.next = node
-        self.tail.prev = node
 
-    def removen(self, node:Node):
-        p = node.prev
-        n = node.next
-        p.next = n
-        n.prev = p
+    def addn(self, node:Node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
     
+    def removen(self, node:Node):
+        p = node
+        q = node.next
+        q.prev = p.prev
+        p.prev.next = q
+
     def get(self, key: int) -> int:
-        if key in self.hash:
-            node = self.hash[key]
-            self.removen(node)
-            self.addn(node)
-            return node.value
+        if key in self.mp:
+            p = self.mp[key]
+            self.removen(p)
+            self.addn(p)
+            return p.val
         else:
             return -1
         
-
     def put(self, key: int, value: int) -> None:
-        if key in self.hash:
-            node = self.hash[key]
-            self.removen(node)
+        if key in self.mp:
+            p = self.mp[key]
+            self.removen(p)
         node = Node(key, value)
-        self.hash[key] = node
         self.addn(node)
-
-        if len(self.hash) > self.capacity:
-            r = self.head.next
-            self.removen(r)
-            del self.hash[r.key]
+        self.mp[key] = node
+        if len(self.mp) > self.capacity:
+            n = self.tail.prev
+            self.removen(n)
+            del self.mp[n.key]
         
 
 
