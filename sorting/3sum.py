0462c7a822333fb8913fba96cd5a49e7eedc02e3
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-09 00:03:07 -0800

    LC-3sum [Runtime: 829 ms; Memory: 20.7 MB]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -3,7 +3,7 @@ class Solution:
         res = []
         nums.sort()
         if nums[0] > 0:
-            return
+            return []
         for i in range(len(nums)):
             if i>0 and nums[i] == nums[i-1]:
                 continue
@@ -18,13 +18,14 @@ class Solution:
                         left += 1
                     while left<right and nums[right] == nums[right-1]:
                         right -= 1
+                    left += 1
+                    right -= 1
                 elif nums[i]+nums[left]+nums[right] > 0:
                     right -= 1
                 else:
                     left += 1
                 
-                left += 1
-                right -= 1
+
         
         return res
             
