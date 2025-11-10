4c912bd98279f93e55de561118459964b19ef719
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-09 16:03:15 -0800

    LC-trapping-rain-water [Runtime: 0 ms; Memory: 19.2 MB]

--- stack/trapping-rain-water.py
+++ stack/trapping-rain-water.py
@@ -1,15 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack = []
         res = 0
-        for i, x in enumerate(height):
-            while stack and height[stack[-1]] < x:
-                mid = stack.pop()
-                if stack:
-                    h = min(height[i], height[stack[-1]])
-                    w = i - stack[-1] - 1
-                    res += (h-height[mid])*w
-            
-            stack.append(i)
-        
+        left = 0
+        right = len(height) -1
+        maxL = 0
+        maxR = 0
+        while left<right:
+            if height[left] < height[right]:
+                if height[left] < maxL:
+                    res += maxL - height[left]
+                else:
+                    maxL = height[left]
+                left += 1
+            else:
+                if height[right] < maxR:
+                    res += maxR - height[right]
+                else:
+                    maxR = height[right]
+                right -= 1
+
         return res
