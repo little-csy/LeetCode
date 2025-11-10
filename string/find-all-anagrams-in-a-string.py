626b22cdf6171a2eef3be0e1f2ab4233c5fdaab6
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-09 20:33:33 -0800

    LC-find-all-anagrams-in-a-string [Runtime: N/A; Memory: N/A]

---
+++ string/find-all-anagrams-in-a-string.py
@@ -0,0 +1,24 @@
+from collections import Counter
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        pmap = Counter(p)
+        smap = {}
+        need = len(pmap)
+        have = 0
+        left = 0
+        res = []
+
+        for right, val in enumerate(s):
+            smap[val] = smap.get(val, 0) + 1
+
+            if val in pmap and smap[val] == pmap[val]:
+                have += 1
+            
+            if right-left+1 == len(p):
+                if have == need:
+                    res.append(left)
+                smap[s[left]] -= 1
+                if smap[s[left]] != pmap[s[left]]:
+                    have -= 1
+                left += 1
+        return res
