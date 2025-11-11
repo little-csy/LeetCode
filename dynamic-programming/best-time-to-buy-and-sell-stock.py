577b3f42498e13e50a646ce1f9a8bd9c9bc69517
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-10 19:42:12 -0800

    LC-best-time-to-buy-and-sell-stock [Runtime: 346 ms; Memory: 32.6 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock.py
@@ -6,4 +6,4 @@ class Solution:
         for i in range(1,len(prices)):
             dp[i][0] = max(dp[i-1][0], -prices[i])
             dp[i][1] = max(dp[i-1][1], dp[i-1][0]+prices[i])
-        return dp[len(prices)-1][1]
+        return max(dp[len(prices)-1][1],dp[len(prices)-1][0])
