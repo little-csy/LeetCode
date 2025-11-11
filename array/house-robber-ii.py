2a89eff6fef00d24df3b59011e636d58e4a5c4a6
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-10 18:39:56 -0800

    LC-house-robber-ii [Runtime: N/A; Memory: N/A]

---
+++ array/house-robber-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        nohead = self.robnoloop(nums[1:])
+        notail = self.robnoloop(nums[:len(nums)-1])
+        return max(nohead, notail)
+    
+    def robnoloop(self, nums:List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums[0], nums[1])
+        else:
+            dp = [0] * len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+            for i in range(2, len(nums)):
+                dp[i] = max(dp[i-1], dp[i-2]+nums[i])
+            return dp[len(nums)-1]
