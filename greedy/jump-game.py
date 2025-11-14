879ae54d10e00fc1e7b15bacb9b83649be8950b7
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-14 14:20:02 -0800

    LC-jump-game [Runtime: N/A; Memory: N/A]

---
+++ greedy/jump-game.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        cover = 0
+        i = 0
+        if len(nums) == 1:
+            return True
+        while i <= cover:
+            cover = max(cover, i+nums[i])
+            if cover == len(nums)-1:
+                return True
+            i+=1
+        return False
