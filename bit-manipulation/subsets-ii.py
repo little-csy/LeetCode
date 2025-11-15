b3c02cd11b20a60ae817570d527a3d31b9c6f4b9
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-15 14:37:14 -0800

    LC-subsets-ii [Runtime: N/A; Memory: N/A]

---
+++ bit-manipulation/subsets-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        path = []
+        def backtracking(nums, start, used):
+            res.append(path[:])
+            if start >= len(nums):
+                return
+            
+            for i in range(start, len(nums)):
+                if i>0 and nums[i] == nums[i-1] and used[i-1] == 0:
+                    continue
+                path.append(nums[i])
+                used[i] = 1
+                backtracking(nums, i+1, used)
+                path.pop()
+                used[i] = 0
+        
+        used = [0]*len(nums)
+        backtracking(nums, 0, used)
+        return res
