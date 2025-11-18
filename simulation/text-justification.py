b7504b236ed7080f094cd065fbfe2d79550bfc8a
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-17 19:21:14 -0800

    LC-text-justification [Runtime: 2 ms; Memory: 18.1 MB]

--- simulation/text-justification.py
+++ simulation/text-justification.py
@@ -1,34 +1,34 @@
 class Solution:
     def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         res = []
-        i = 0 
+        i = 0
         while i < len(words):
             last = i+1
             line_len = len(words[i])
-            while last<len(words) and line_len+1+len(words[last])<=maxWidth:
-                line_len+=len(words[last])+1
+
+            while last<len(words) and line_len + 1 + len(words[last]) <= maxWidth:
+                line_len += len(words[last]) + 1
                 last += 1
             
             line_words = words[i:last]
             gap = len(line_words)-1
 
             if gap == 0 or last == len(words):
-                line = " ".join(line_words)
-                line+= " "*(maxWidth-len(line))
+                line =" ".join(line_words)
+                pad = maxWidth - len(line)
+                line += " "*pad
             else:
-                total = 0
+                total_sum = 0
                 for k in range(len(line_words)):
-                    total += len(line_words[k])
-                total_pad = maxWidth - total
-                each_pad = total_pad//gap
-                more_pad = total_pad % gap
-                line = ""
+                    total_sum+=len(line_words[k])
+                each_pad = (maxWidth-total_sum)//gap
+                more_pad = (maxWidth-total_sum)%gap
+                line=""
                 for m in range(gap):
-                    line+=line_words[m]
                     if m<more_pad:
-                        line+=" "*(1+each_pad)
+                        line+=line_words[m]+" "*(each_pad+1)
                     else:
-                        line+=" "*(each_pad)
+                        line+=line_words[m]+" "*(each_pad)
                 line+=line_words[-1]
             res.append(line)
             i = last
