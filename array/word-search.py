ed3656ee73d5c77f634e94aa08d65306f9f4ec7e
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-18 15:56:03 -0800

    LC-word-search [Runtime: N/A; Memory: N/A]

--- array/word-search.py
+++ array/word-search.py
@@ -1,28 +1,17 @@
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        dx = [-1,0,1,0]
-        dy = [0,1,0,-1]
-
-        def dfs(board, word, index, x, y):
-            if board[x][y] != word[index]:
-                return False
-            if len(word)-1 == index:
+    def dfs(i:int, j:int, index:int) -> bool:
+            if len(word) == index:
                 return True
-            
-            temp = board[x][y]
-            board[x][y] = '#'
-            for i in range(4):
-                nx = x+dx[i]
-                ny = y+dy[i]
-                if nx<0 or nx>=len(board) or ny<0 or ny>=len(board[0]) or board[nx][ny] == '#':
-                    continue
-                if (dfs(board,word,index+1,nx,ny)):
-                    return True
-            board[x][y] = temp
-            return False
-
+            if i<0 or i>= len(board) or j<0 or j>=len(board[0]) or word[index] != board[i][j]:
+                return False
+            temp = board[i][j]
+            board[i][j] = '#'
+            found = dfs(i+1,j,index+1) or dfs(i-1,j,index+1) or dfs(i,j-1,index+1) or dfs(i,j+1,index+1)
+            board[i][j] = temp
+            return found
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if dfs(board, word, 0, i, j):
+                if dfs(i,j,0):
                     return True
         return False
