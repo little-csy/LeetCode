0559b04d39a4fc79d37bc081ef268f9da5f0bce6
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-17 19:47:18 -0800

    LC-number-of-islands [Runtime: 242 ms; Memory: 20.1 MB]

--- breadth-first-search/number-of-islands.py
+++ breadth-first-search/number-of-islands.py
@@ -5,15 +5,14 @@ class Solution:
             for j in range(len(grid[0])):
                 if grid[i][j] == '1':
                     cnt+=1
-                    self.dfs(grid, i, j)
+                    self.dfs(grid,i,j)
         return cnt
     
-    def dfs(self, grid:List[List[str]], i:int, j:int) -> None:
+    def dfs(self, grid: List[List[str]], i: int, j: int):
         if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] == '0':
             return
-        else:
-            grid[i][j] = '0'
-            self.dfs(grid, i-1,j)
-            self.dfs(grid, i+1,j)
-            self.dfs(grid, i,j-1)
-            self.dfs(grid, i,j+1)
+        grid[i][j] = '0'
+        self.dfs(grid, i+1,j)
+        self.dfs(grid, i-1,j)
+        self.dfs(grid, i,j-1)
+        self.dfs(grid, i,j+1)
