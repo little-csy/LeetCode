2c9576152b0c72c1ba71c5a5530a6d673b8b98a9
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-17 21:15:52 -0800

    LC-meeting-rooms-ii [Runtime: N/A; Memory: N/A]

--- sorting/meeting-rooms-ii.py
+++ sorting/meeting-rooms-ii.py
@@ -1,18 +1,10 @@
+import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        event = []
-        for start,end in intervals:
-            event.append((start, +1))
-            event.append((end, -1))
-
-        event.sort(key = lambda x: (x[0],x[1]))
-
-        cur = 0
-        best = 0
-
-        for i in range(len(event)):
-            cur+=event[i][1]
-            if cur>best:
-                best = cur
-        
-        return best
+        intervals.sort(key=lambda x:x[0])
+        heap = []
+        for s, e in intervals:
+            while heap and s>heap[0]:
+                heapq.heappop(heap)
+            heapq.heappush(heap, e)
+        return len(heap)
