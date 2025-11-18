917577fa5ed83e082bd5b5caa09131e84d154ac3
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-18 15:54:35 -0800

    LC-number-of-islands [Runtime: 248 ms; Memory: 20.2 MB]

--- breadth-first-search/number-of-islands.py
+++ breadth-first-search/number-of-islands.py
@@ -1,7 +1,5 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
         cnt = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
