960acb96f260d0974a6e8e7031fa31101aa1b459
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-17 21:17:00 -0800

    LC-meeting-rooms-ii [Runtime: N/A; Memory: N/A]

--- array/meeting-rooms-ii.py
+++ array/meeting-rooms-ii.py
@@ -4,7 +4,7 @@ class Solution:
         intervals.sort(key=lambda x:x[0])
         heap = []
         for s, e in intervals:
-            while heap and s>heap[0]:
+            while heap and s>=heap[0]:
                 heapq.heappop(heap)
             heapq.heappush(heap, e)
         return len(heap)
