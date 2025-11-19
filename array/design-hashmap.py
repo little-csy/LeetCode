c89d12dbc51a5d55978e4740d50db32060aca6ee
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-18 18:27:19 -0800

    LC-design-hashmap [Runtime: 55 ms; Memory: 43.4 MB]

---
+++ array/design-hashmap.py
@@ -0,0 +1,22 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.size = 10**6+1
+        self.data = [-1] * self.size
+
+    def put(self, key: int, value: int) -> None:
+        self.data[key] = value
+
+    def get(self, key: int) -> int:
+        return self.data[key]
+        
+    def remove(self, key: int) -> None:
+        self.data[key] = -1
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
