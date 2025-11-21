254f598e9f62b45c81ffa8a023dcaf7c0513afd8
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-20 18:06:48 -0800

    LC-max-area-of-island [Runtime: 20 ms; Memory: 18.1 MB]

---
+++ array/max-area-of-island.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(grid, i, j):
+            if i <0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            area = 1
+            area+= dfs(grid, i+1,j)
+            area+= dfs(grid, i-1,j)
+            area+= dfs(grid, i, j-1)
+            area+= dfs(grid, i, j+1)
+            return area
+        
+        maxarea = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                maxarea = max(dfs(grid, i, j),maxarea)
+        
+        return maxarea
