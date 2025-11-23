aa0d8a954397785f50d1dc5d00380d20d3d650d5
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-23 15:37:59 -0800

    LC-meeting-rooms [Runtime: N/A; Memory: N/A]

--- array/meeting-rooms.py
+++ array/meeting-rooms.py
@@ -3,9 +3,12 @@ class Solution:
         intervals.sort(key=lambda x:x[0])
         stack = []
         for s,e in intervals:
-            if not stack:
-                stack.append(e)
-                continue
-            if s<stack[-1]:
-                return False
-        return True
+            if not stack or s>stack[-1][1]:
+                stack.append([s,e])
+            else:
+                stack[-1][1] = max(stack[-1][1], e)
+                
+        if len(intervals) == len(stack):
+            return True
+        else:
+            return False
