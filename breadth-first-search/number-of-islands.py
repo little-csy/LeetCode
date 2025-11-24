6ed45b3e530645b64ad4cd4a7915529185a32364
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-24 10:42:48 -0800

    LC-number-of-islands [Runtime: 239 ms; Memory: 20.2 MB]

--- breadth-first-search/number-of-islands.py
+++ breadth-first-search/number-of-islands.py
@@ -1,14 +1,14 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(grid, i, j: int):
+        def dfs(grid, i, j):
             if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] == '0':
                 return
             grid[i][j] = '0'
-            dfs(grid, i+1,j)
-            dfs(grid, i-1,j)
-            dfs(grid, i,j-1)
-            dfs(grid, i,j+1)
-
+            dfs(grid, i+1, j)
+            dfs(grid, i-1, j)
+            dfs(grid, i, j-1) 
+            dfs(grid, i, j+1)
+        
         cnt = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
@@ -16,5 +16,6 @@ class Solution:
                     cnt+=1
                     dfs(grid,i,j)
         return cnt
+
     
     
