f73581f712c2ff4309905177ee71d3f0be970350
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-24 10:38:01 -0800

    LC-simplify-path [Runtime: 3 ms; Memory: 17.8 MB]

--- stack/simplify-path.py
+++ stack/simplify-path.py
@@ -2,12 +2,14 @@ class Solution:
     def simplifyPath(self, path: str) -> str:
         part = path.split('/')
         stack = []
+
         for w in part:
-            if w == '.' or w == '':
+            if w=='.' or w=='':
                 continue
-            elif w == '..':
+            elif w=='..':
                 if stack:
                     stack.pop()
             else:
                 stack.append(w)
+        
         return '/' + '/'.join(stack)
