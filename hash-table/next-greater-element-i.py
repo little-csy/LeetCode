6a48a957b886cc75d8f9e92ad62d3f37ead226cd
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2025-11-23 16:54:13 -0800

    LC-next-greater-element-i [Runtime: 20 ms; Memory: 18.1 MB]

---
+++ hash-table/next-greater-element-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+        for num1 in nums1:
+            i = nums2.index(num1)
+            found = -1
+            for j in range(i+1,len(nums2)):
+                if nums2[j]>num1:
+                    found = nums2[j]
+                    break
+            res.append(found)
+        return res
+
+        
