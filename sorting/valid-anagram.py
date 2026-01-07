9e51c99cdbd68c7195eb1ff9ed6009374f1b2db4
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-06 22:30:21 -0800

    LC-valid-anagram [Runtime: 12 ms; Memory: 19.3 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,11 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hasharray = [0] * 26
-        for i in range(len(s)):
-            hasharray[ord(s[i])-ord('a')] += 1
-        for i in range(len(t)):
-            hasharray[ord(t[i])-ord('a')] -= 1
-        for i in range(26):
-            if hasharray[i] !=0:
+        cnt = [0] * 26
+        for sch in s:
+            cnt[ord(sch)-ord('a')]+=1
+        for tch in t:
+            cnt[ord(tch)-ord('a')]-=1
+        for ch in cnt:
+            if ch != 0:
                 return False
-        return True
+        return True 
