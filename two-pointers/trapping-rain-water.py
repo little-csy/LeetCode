22edd10532c112a91bc684911aa4ade54ac0c4a0
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-08 20:17:48 -0800

    LC-trapping-rain-water [Runtime: 6 ms; Memory: 21.2 MB]

--- two-pointers/trapping-rain-water.py
+++ two-pointers/trapping-rain-water.py
@@ -2,21 +2,21 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         res = 0
         left = 0
-        right = len(height) -1
-        maxL = 0
-        maxR = 0
-        while left<right:
-            if height[left] < height[right]:
-                if height[left] < maxL:
-                    res += maxL - height[left]
+        right = len(height)-1
+        maxl = 0
+        maxr = 0
+        while left<=right:
+            if height[left] <= height[right]:
+                if height[left] >= maxl:
+                    maxl = height[left]
                 else:
-                    maxL = height[left]
+                    res += maxl - height[left]
                 left += 1
             else:
-                if height[right] < maxR:
-                    res += maxR - height[right]
+                if height[right] >= maxr:
+                    maxr = height[right]
                 else:
-                    maxR = height[right]
+                    res += maxr - height[right]
                 right -= 1
-
+        
         return res
