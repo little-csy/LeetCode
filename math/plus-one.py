e34474b55caa664d52c2249a8b65d7124d7edccf
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-09 17:40:26 -0800

    LC-plus-one [Runtime: 0 ms; Memory: 19.3 MB]

--- math/plus-one.py
+++ math/plus-one.py
@@ -1,5 +1,8 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        new = ''.join(map(str, digits))
-        num = int(new)
-        return list(map(int, str(num+1)))
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i]+=1 
+                return digits
+            digits[i] = 0
+        return [1]+digits
