1c85b578a241cc2497be1d83546e15b78ed32240
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-10 01:07:22 -0800

    LC-subarray-sum-equals-k [Runtime: 36 ms; Memory: 21.9 MB]

--- hash-table/subarray-sum-equals-k.py
+++ hash-table/subarray-sum-equals-k.py
@@ -1,11 +1,12 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        n = len(nums)
-        ans = 0
-        for i in range(n):
-            res = 0
-            for j in range(i, n):
-                res += nums[j]
-                if res == k:
-                    ans+=1
-        return ans
+        mp = defaultdict(int)
+        mp[0] = 1
+        res = 0
+        pre = 0
+        for n in nums:
+            pre += n
+            if pre-k in mp:
+                res+=mp[pre-k]
+            mp[pre] += 1
+        return res
