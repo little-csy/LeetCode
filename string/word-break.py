961168d0c312e4084cefed0eb97f798684319af5
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-09 18:56:17 -0800

    LC-word-break [Runtime: 5 ms; Memory: 19.3 MB]

--- string/word-break.py
+++ string/word-break.py
@@ -1,15 +1,10 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordSet = set(wordDict)
-
-        def dfs(start):
-            if start == len(s):
-                return True
-            
-            for i in range(start+1,len(s)+1):
-                if s[start:i] in wordSet:
-                    if dfs(i):
-                        return True
-            return False
-        
-        return dfs(0)
+        dp = [False] * (len(s)+1)
+        dp[0] = True
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                if dp[j] == True and s[j:i] in wordDict:
+                    dp[i] = True
+        return dp[len(s)]
