1b956214358f5f9e0d73710a3dc3b9a8c34905da
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-12 15:41:22 -0800

    LC-reverse-integer [Runtime: 43 ms; Memory: 19.5 MB]

--- math/reverse-integer.py
+++ math/reverse-integer.py
@@ -1,7 +1,5 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x < -1*2**31 or x > 2**31-1:
-            return 0
         if x>0:
             flag = 1
         else:
@@ -11,5 +9,8 @@ class Solution:
         while absx > 0:
             re = re*10 + absx%10
             absx = absx // 10
+        reverse = flag*re 
+        if reverse < -1*2**31 or reverse > 2**31-1:
+            return 0
         
-        return flag*re
+        return reverse
