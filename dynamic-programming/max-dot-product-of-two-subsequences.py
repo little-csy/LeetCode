51077d918693f7380b8c8fc94d26ea9da0d62684
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-13 14:19:21 -0800

    LC-max-dot-product-of-two-subsequences [Runtime: N/A; Memory: N/A]

---
+++ dynamic-programming/max-dot-product-of-two-subsequences.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
+        dp = [[-10**4]*(len(nums1)+1) for _ in range(len(nums2)+1)]
+
+        for i in range(1, len(nums2)+1):
+            for j in range(1, len(nums1)+1):
+                dp[i][j] = max(dp[i-1][j-1]+nums2[i-1]*nums1[j-1], nums2[i-1]*nums1[j-1], dp[i-1][j], dp[i][j-1])
+        
+        return dp[i][j]
