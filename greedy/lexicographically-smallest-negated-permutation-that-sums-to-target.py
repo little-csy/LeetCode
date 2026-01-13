db7f9548c4ac8c3c07010fdf2a4690618f877a9d
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-13 13:35:49 -0800

    LC-lexicographically-smallest-negated-permutation-that-sums-to-target [Runtime: N/A; Memory: N/A]

--- greedy/lexicographically-smallest-negated-permutation-that-sums-to-target.py
+++ greedy/lexicographically-smallest-negated-permutation-that-sums-to-target.py
@@ -2,8 +2,7 @@ class Solution:
     def lexSmallestNegatedPerm(self, n: int, target: int) -> List[int]:
         sumn = 0
         res = []
-        for i in range(1,n+1):
-            sumn+=i
+        sumn = (1+n)*n//2
         if (sumn-target) %2 == 1:
             return []
         sumneg = (sumn-target)//2
