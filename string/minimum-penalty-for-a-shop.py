fbdb3de30ff7ca96fc3cea017b3fd6a3e317fcd7
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-12 22:22:40 -0800

    LC-minimum-penalty-for-a-shop [Runtime: 75 ms; Memory: 20.1 MB]

--- string/minimum-penalty-for-a-shop.py
+++ string/minimum-penalty-for-a-shop.py
@@ -1,18 +1,24 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
-        minp = float('inf')
-        minh = -1
+        p = 0
+        
+        for c in customers:
+            if c=='Y':
+                p += 1
+        
+        besth = 0
+        bestp = p
         n = len(customers)
-        for t in range(n+1):
-            p = 0
-            for i in range(t):
-                if customers[i] == 'N':
-                    p+=1
+        
+        for i in range(n):
+            if customers[i] == 'Y':
+                p -= 1
             
-            for j in range(t,n):
-                if customers[j] == 'Y':
-                    p+=1
-            if p<minp:
-                minp = p
-                minh=t
-        return minh
+            if customers[i] == 'N':
+                p += 1
+            
+            if bestp > p:
+                bestp = p
+                besth = i+1
+        
+        return besth
