6cccee688702b910008854e221aaaf1b23367c7f
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-13 15:55:51 -0800

    LC-letter-combinations-of-a-phone-number [Runtime: 0 ms; Memory: 19.3 MB]

--- backtracking/letter-combinations-of-a-phone-number.py
+++ backtracking/letter-combinations-of-a-phone-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        path = []
         s = {
             "0":"",
             "1":"",
@@ -11,18 +13,18 @@ class Solution:
             "7":"pqrs",
             "8":"tuv",
             "9":"wxyz",
-            }
-        res = []
-        path = []
+        }
 
-        def backtrack(digits, start):
+        def dfs(digits, start):
             if len(path) == len(digits):
-                res.append("".join(path))
+                res.append(''.join(path))
                 return
+            
             for val in s[digits[start]]:
                 path.append(val)
-                backtrack(digits, start+1)
+                dfs(digits, start+1)
                 path.pop()
         
-        backtrack(digits, 0)
+        dfs(digits, 0)
+        
         return res
