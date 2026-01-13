fcd15d906a95e8af6718aaad63aa3003cb86dd54
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-12 16:25:30 -0800

    LC-word-ladder [Runtime: N/A; Memory: N/A]

--- breadth-first-search/word-ladder.py
+++ breadth-first-search/word-ladder.py
@@ -1,25 +1,33 @@
-from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordlistset = set(wordList)
-        if endWord not in wordlistset:
+        wordSet = set(wordList)
+
+        if endWord not in wordSet:
             return 0
-        visit = set()
-        visit.add(beginWord)
-        q= deque()
-        q.append((beginWord,1))
+        
+        front = {beginWord}
+        back = {endWord}
+        step = 1
+        L = len(beginWord)
 
-        while q:
-            word, step = q.popleft()
+        while front and back:
+            if len(front) > len(back):
+                front, back = back, front
+            
+            nxt = set()
+            
+            for s in front:
+                for i in range(L):
+                    for v in "abcdefghijklmnopqrstuvwxyz":
+                        news = s[:i] + v + s[i+1:]
 
-            if word == endWord:
-                return step
-
-            for i in range(len(word)):
-                for c in "abcdefghijklmnopqrstuvwxyz":
-                    newword = word[:i] + c + word[i+1:]
-                    if newword in wordlistset and newword not in visit:
-                        visit.add(newword)
-                        q.append((newword,step+1)) 
-
-        return 0                      
+                        if news in wordSet:
+                            nxt.add(news)
+                            wordSet.remove(news)
+                        
+                        if news in back:
+                            return step+1
+            front = nxt
+            step += 1
+        return step
+                        
