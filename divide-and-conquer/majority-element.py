c773bc627d27206a4871e1b75abed5f2219f148e
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-13 19:40:03 -0800

    LC-majority-element [Runtime: 3 ms; Memory: 21.2 MB]

--- divide-and-conquer/majority-element.py
+++ divide-and-conquer/majority-element.py
@@ -1,10 +1,15 @@
-from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        mtime = len(nums)//2
-
-        hp = Counter(nums)
-
-        for val, freq in hp.items():
-            if freq > mtime:
-                return val
+        cand = 0
+        cnt = 0
+        for x in nums:
+            if cnt == 0:
+                cand = x
+                cnt+=1
+            else:
+                if x==cand:
+                    cnt+=1
+                else:
+                    cnt-=1
+        
+        return cand
