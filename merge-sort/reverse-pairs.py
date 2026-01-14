d642fd0d3ca903d37d0b134bad13559343ac3680
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-14 15:52:27 -0800

    LC-reverse-pairs [Runtime: N/A; Memory: N/A]

---
+++ merge-sort/reverse-pairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        cnt = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] > 2 * nums[j]:
+                    cnt += 1
+        
+        return cnt
