6a23675a111997b45f4cbfad9cbe5e182d594944
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-14 19:00:00 -0800

    LC-4sum [Runtime: 611 ms; Memory: 19.6 MB]

--- sorting/4sum.py
+++ sorting/4sum.py
@@ -1,36 +1,34 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
-        result = []
+        res = []
         for i in range(len(nums)):
-            if nums[i]>target and target>=0:
-                return result
-            if i>0 and nums[i]==nums[i-1]:
+            if i>0 and nums[i] == nums[i-1]:
                 continue
+
             for j in range(i+1, len(nums)):
-                if nums[i]+nums[j]>target and target>=0:
-                    break
-                if j>i+1 and nums[j]==nums[j-1]:
-                    continue
+
                 left = j+1
                 right = len(nums)-1
+
+                if j>i+1 and nums[j] == nums[j-1]:
+                    continue
+                
                 while left<right:
-                    if nums[i]+nums[j]+nums[left]+nums[right] > target:
-                        right -= 1
-                    elif nums[i]+nums[j]+nums[left]+nums[right] < target:
-                        left += 1
-                    else:
-                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                    if nums[i]+nums[j]+nums[left]+nums[right] == target:
+                        res.append([nums[i], nums[j], nums[left], nums[right]])
 
                         while left<right and nums[left] == nums[left+1]:
-                            left += 1
+                            left+=1
                         while left<right and nums[right] == nums[right-1]:
-                            right -= 1
+                            right-=1
                         
-                        left += 1
+                        left+=1
+                        right-=1
+                    
+                    elif nums[i]+nums[j]+nums[left]+nums[right] > target:
                         right -= 1
-        
-        return result
-
-
-        
+                    
+                    else:
+                        left += 1
+        return res
