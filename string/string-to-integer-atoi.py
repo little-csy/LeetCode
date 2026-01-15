be4912aea06e971c432ebdcb5a7c6c0f140e0ec6
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-14 18:15:43 -0800

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

---
+++ string/string-to-integer-atoi.py
@@ -0,0 +1,30 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        i = 0
+        n = len(s)
+
+        while i<n and s[i] == ' ':
+            i+=1
+        
+        sig = 1
+        if s[i]=='+':
+            sig = 1
+            i+=1
+        if s[i]=='-':
+            sig = -1
+            i+=1
+        
+        num = 0
+        INTMAX = 2**31-1
+        INTMIN = -2**31
+        while i<n and s[i].isdigit():
+            digit = ord(s[i])-ord('0')
+            if num>INTMAX//10 or (num == INTMAX//10 and digit>INTMAX%10):
+                if sig==1:
+                    return INTMAX
+                else:
+                    return INTMIN
+            num = num*10 + digit
+            i+=1
+        
+        return sig*num
