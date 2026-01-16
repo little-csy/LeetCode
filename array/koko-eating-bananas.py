9a1b476defcebd13ae53a7e8bb0c1b9b66d629c5
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-15 19:50:30 -0800

    LC-koko-eating-bananas [Runtime: 183 ms; Memory: 20.6 MB]

---
+++ array/koko-eating-bananas.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles)
+
+        while l<r:
+            hour = 0
+            k = (l+r)//2
+
+            for p in piles:
+                if p%k == 0:
+                    hour+=p//k
+                else:
+                    hour+=p//k+1
+            
+            if hour<=h:
+                r = k
+            else:
+                l = k+1
+        
+        return l
