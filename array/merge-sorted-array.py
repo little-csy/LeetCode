547d6f0ca08e363430046070b38739010dcd5d15
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-15 16:33:00 -0800

    LC-merge-sorted-array [Runtime: 0 ms; Memory: 19.5 MB]

--- array/merge-sorted-array.py
+++ array/merge-sorted-array.py
@@ -8,7 +8,7 @@ class Solution:
         k = m+n-1
 
         while j>=0:
-            if nums2[j]>=nums1[i]:
+            if i<0 or nums2[j]>=nums1[i]:
                 nums1[k] = nums2[j]
                 j -= 1
                 k -= 1
