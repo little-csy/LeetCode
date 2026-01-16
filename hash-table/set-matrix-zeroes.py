a85d6cd0ef93c56fd46eb5bbc5d43ee672e01deb
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-01-15 18:39:26 -0800

    LC-set-matrix-zeroes [Runtime: 4 ms; Memory: 20.1 MB]

--- hash-table/set-matrix-zeroes.py
+++ hash-table/set-matrix-zeroes.py
@@ -3,19 +3,19 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        seen = set()
+        seenr = set()
+        seenc = set()
 
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    seen.add(i)
-                    seen.add(j)
+                    seenr.add(i)
+                    seenc.add(j)
         
-        for val in seen:
-
-            if val < len(matrix[0]):
-                for i in range(len(matrix)):
-                    matrix[i][val] = 0
-            if val < len(matrix):
-                for j in range(len(matrix[0])):
-                    matrix[val][j] = 0
+        for val in seenr:
+            for j in range(len(matrix[0])):
+                matrix[val][j] = 0
+        
+        for val in seenc:
+            for i in range(len(matrix)):
+                matrix[i][val] = 0
