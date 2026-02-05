4e0f84e52651b898da98057cf2a488a278838798
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-02-05 15:51:37 -0800

    LC-minimum-window-substring [Runtime: 83 ms; Memory: 19.7 MB]

--- hash-table/minimum-window-substring.py
+++ hash-table/minimum-window-substring.py
@@ -1,32 +1,31 @@
-from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         tmp = Counter(t)
-        smp = Counter()
         require = len(tmp)
+        smp = Counter()
         vaild = 0
-        l = 0
         start = 0
-        slen = float('inf')
+        minlen = float('inf')
+        l = 0
 
         for r, ch in enumerate(s):
             if ch in tmp:
                 smp[ch] += 1
                 if smp[ch] == tmp[ch]:
-                    vaild +=1
+                    vaild += 1
             
-            while vaild == require:
-                if r-l+1<slen:
-                    slen = r-l+1
+            while require == vaild:
+                if r-l+1<minlen:
+                    minlen = r-l+1
                     start = l
-                
+
                 if s[l] in tmp:
-                    if smp[s[l]] == tmp[s[l]]:
+                    if tmp[s[l]] == smp[s[l]]:
                         vaild -= 1
                     smp[s[l]] -= 1
                 l+=1
-        
-        if slen == float('inf'):
+
+        if minlen == float('inf'):
             return ""
         else:
-            return s[start:start+slen]
+            return s[start:start+minlen]
