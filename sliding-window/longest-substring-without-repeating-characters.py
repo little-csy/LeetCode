3e40b65a7a172d27688a0c93e6f6b5cb36070ed4
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-02-05 17:02:44 -0800

    LC-longest-substring-without-repeating-characters [Runtime: 11 ms; Memory: 19.4 MB]

--- sliding-window/longest-substring-without-repeating-characters.py
+++ sliding-window/longest-substring-without-repeating-characters.py
@@ -5,9 +5,9 @@ class Solution:
         maxl = 0
         for r, ch in enumerate(s):
             while s[r] in mp :
-                if r-l > maxl:
-                    maxl = r-l
                 mp.remove(s[l])
                 l+=1
             mp.add(ch)
+            if r-l+1 > maxl:
+                    maxl = r-l+1
         return maxl
