8b2cf90998284ae0f16aefd4b1083889d9fc8feb
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-02-08 16:29:52 -0800

    LC-minimum-window-substring [Runtime: 79 ms; Memory: 19.9 MB]

--- sliding-window/minimum-window-substring.py
+++ sliding-window/minimum-window-substring.py
@@ -1,31 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         tmp = Counter(t)
-        require = len(tmp)
         smp = Counter()
+        minlen = float("inf")
+        require = len(tmp)
         vaild = 0
         start = 0
-        minlen = float('inf')
         l = 0
 
         for r, ch in enumerate(s):
             if ch in tmp:
-                smp[ch] += 1
+                smp[ch]+=1
                 if smp[ch] == tmp[ch]:
-                    vaild += 1
+                    vaild +=1
             
             while require == vaild:
                 if r-l+1<minlen:
                     minlen = r-l+1
                     start = l
-
+                
                 if s[l] in tmp:
-                    if tmp[s[l]] == smp[s[l]]:
+                    if smp[s[l]] == tmp[s[l]]:
                         vaild -= 1
-                    smp[s[l]] -= 1
+                    smp[s[l]]-=1
                 l+=1
-
-        if minlen == float('inf'):
+        
+        if minlen == float("inf"):
             return ""
         else:
             return s[start:start+minlen]
