c2af25e93d81311733c5607e6dda987ef48553f2
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-02-23 15:59:29 -0800

    LC-minimum-size-subarray-sum [Runtime: 14 ms; Memory: 30.8 MB]

--- binary-search/minimum-size-subarray-sum.py
+++ binary-search/minimum-size-subarray-sum.py
@@ -1,16 +1,15 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        i = 0
-        sums = 0
-        result = float('inf')
-        for j in range(len(nums)):
-            sums += nums[j]
-            while(sums >= target):
-                result = min(result, j-i+1)
-                sums -= nums[i]
-                i += 1
-        
-        if result == float('inf'):
+        minlen = float('inf')
+        l = 0
+        sumc = 0
+        for r, c in enumerate(nums):
+            sumc += c
+            while sumc>=target:
+                minlen = min(minlen, r-l+1)
+                sumc -= nums[l]
+                l+=1
+        if minlen == float('inf'):
             return 0
         else:
-            return result
+            return minlen
