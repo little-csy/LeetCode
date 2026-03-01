6ed7bc7c489812ec91c7a597618b37b11ba21a2d
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-02-28 21:54:18 -0800

    LC-longest-palindromic-substring [Runtime: 220 ms; Memory: 19.4 MB]

--- string/longest-palindromic-substring.py
+++ string/longest-palindromic-substring.py
@@ -3,8 +3,9 @@ class Solution:
         res = []
 
         for i in range(len(s)):
-            l = i-1
-            r = i+1
+            l=i-1
+            r=i+1
+
             while l>=0 and r<len(s):
                 if s[l] == s[r]:
                     l-=1
@@ -17,14 +18,14 @@ class Solution:
             
             l = i
             r = i+1
+
             while l>=0 and r<len(s):
                 if s[l] == s[r]:
                     l-=1
                     r+=1
                 else:
                     break
-            
+
             if len(res)<r-l-1:
                 res = s[l+1:r]
-        
         return res
