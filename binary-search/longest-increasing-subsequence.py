8436fcd70e60d437121846982e0bf1b493d70591
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-01 21:26:35 -0800

    LC-longest-increasing-subsequence [Runtime: 11 ms; Memory: 19.6 MB]

--- binary-search/longest-increasing-subsequence.py
+++ binary-search/longest-increasing-subsequence.py
@@ -1,16 +1,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         tail = []
+
         for x in nums:
+            pos = len(tail)
+
             l = 0
             r = len(tail)-1
-            pos = len(tail)
 
             while l<=r:
                 mid = (l+r)//2
-                if tail[mid]>=x:
+                if tail[mid] >= x:
                     pos = mid
-                    r = mid-1
+                    r = mid -1
                 else:
                     l = mid+1
             
@@ -18,5 +20,4 @@ class Solution:
                 tail.append(x)
             else:
                 tail[pos] = x
-        
         return len(tail)
