cdbd6082056c9bf1b91d25b0c3daa6969be7a110
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-01 21:41:34 -0800

    LC-number-of-distinct-islands [Runtime: 31 ms; Memory: 21.4 MB]

--- hash-function/number-of-distinct-islands.py
+++ hash-function/number-of-distinct-islands.py
@@ -1,25 +1,31 @@
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
-        def dfs(grid, i, j , rx, ry, visit, res):
-            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]):
-                return
-            if grid[i][j] == 0 or (i,j) in visit:
+        m = len(grid)
+        n = len(grid[0])
+        visit = [[False]*n for _ in range(m)]
+        shapes = set()
+
+        def dfs(i,j,rx,ry,res):
+
+            if i<0 or i>=m or j<0 or j>=n:
                 return
             
-            visit.add((i,j))
+            if grid[i][j] == 0 or visit[i][j]:
+                return
+            
+            visit[i][j] = True
+
             res.append((i-rx,j-ry))
 
-            dfs(grid, i+1,j,rx,ry,visit,res)
-            dfs(grid, i-1,j,rx,ry,visit,res)
-            dfs(grid, i,j+1,rx,ry,visit,res)
-            dfs(grid, i,j-1,rx,ry,visit,res)
+            dfs(i-1,j,rx,ry,res)
+            dfs(i+1,j,rx,ry,res)
+            dfs(i,j-1,rx,ry,res)
+            dfs(i,j+1,rx,ry,res)
         
-        visit = set()
-        shape = set()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1 and (i,j) not in visit:
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and not visit[i][j]:
                     res = []
-                    dfs(grid, i, j, i, j, visit, res)
-                    shape.add(tuple(res))
-        return len(shape)
+                    dfs(i,j,i,j,res)
+                    shapes.add(tuple(res))
+        return len(shapes)            
