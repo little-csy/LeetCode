fa0cca59a02607254ce291e8e6e46dac6238f8f4
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-02 14:19:58 -0800

    LC-valid-parenthesis-string [Runtime: 0 ms; Memory: 19.3 MB]

--- stack/valid-parenthesis-string.py
+++ stack/valid-parenthesis-string.py
@@ -1,18 +1,20 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        low = 0 
         high = 0
-        for c in s:
-            if c == '(':
+        low = 0
+        for w in s:
+            if w == '(':
                 low += 1
                 high += 1
-            elif c == ')':
+            elif w == ')':
                 low -= 1
-                high -= 1
+                high -=1
             else:
-                low -= 1
                 high += 1
+                low -= 1
+            
             if high<0:
                 return False
-            low = max(0,low)
+            low = max(0, low)
+        
         return low == 0
