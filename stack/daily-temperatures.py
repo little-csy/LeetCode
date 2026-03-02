4a5898161be912dfb1d703382ecc545abe8ecca1
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-02 15:47:33 -0800

    LC-daily-temperatures [Runtime: 95 ms; Memory: 28.2 MB]

--- stack/daily-temperatures.py
+++ stack/daily-temperatures.py
@@ -3,10 +3,11 @@ class Solution:
         n = len(temperatures)
         res = [0] * n
         stack = []
+
         for i, x in enumerate(temperatures):
             while stack and temperatures[stack[-1]] < x:
                 idx = stack.pop()
                 res[idx] = i - idx
-            
             stack.append(i)
+        
         return res
