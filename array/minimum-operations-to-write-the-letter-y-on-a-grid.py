72681151e7655201e3a358fbb20d6f1fcca8b299
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-02 16:09:09 -0800

    LC-minimum-operations-to-write-the-letter-y-on-a-grid [Runtime: 35 ms; Memory: 19.5 MB]

--- array/minimum-operations-to-write-the-letter-y-on-a-grid.py
+++ array/minimum-operations-to-write-the-letter-y-on-a-grid.py
@@ -1,32 +1,33 @@
 class Solution:
     def minimumOperationsToWriteY(self, grid: List[List[int]]) -> int:
+        county = [0,0,0]
+        countny = [0,0,0]
         n = len(grid)
         mid = n//2
-        countY = [0,0,0]
-        ncountY = [0,0,0]
 
         for i in range(n):
             for j in range(n):
-                isY = (
-                    (i<=mid and i==j)or
-                    (i<=mid and i==n-j-1)or
-                    (i>mid and j==mid)
-                )
-                if isY:
-                    countY[grid[i][j]]+=1
+                if (i<=mid and i==j) or (i<=mid and i == n-j-1) or (i>mid and j==mid):
+                    isY = True
                 else:
-                    ncountY[grid[i][j]]+=1
+                    isY = False
+                
+                if isY:
+                    county[grid[i][j]] += 1
+                else:
+                    countny[grid[i][j]] += 1
         
-        totaly = sum(countY)
-        ntotaly = sum(ncountY)
+        totaly = sum(county)
+        totalny = sum(countny)
         res = float('inf')
+
         for c1 in range(3):
             for c2 in range(3):
                 if c1 == c2:
                     continue
-                turny = totaly-countY[c1]
-                nturny = ntotaly-ncountY[c2]
-                res = min(res, turny+nturny)
+                turny = totaly - county[c1]
+                turnny = totalny - countny[c2]
 
+                res = min(res, turny+turnny)
+        
         return res
-                
