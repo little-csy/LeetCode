5730965c915160e0dfc4d9008f25b050f137db62
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-02 19:15:06 -0800

    LC-longest-mountain-in-array [Runtime: N/A; Memory: N/A]

---
+++ array/longest-mountain-in-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestMountain(self, arr: List[int]) -> int:
+        n = len(arr)
+        res = 0
+
+        for i in range(1,n-1):
+            if arr[i-1] < arr[i] > arr[i+1]:
+                l = i-1
+                r = i+1
+
+                while l>=0 and arr[l-1]<arr[l]:
+                    l-=1
+                
+                while r<n and arr[r+1]<arr[r]:
+                    r+=1
+                
+                if r-l+1>res:
+                    res = r-l+1
+        return res
