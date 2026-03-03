7fde63dc6867e06b8cfde856ec69a35f47caad1e
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-02 16:14:05 -0800

    LC-longest-palindromic-substring [Runtime: 207 ms; Memory: 19.2 MB]

--- two-pointers/longest-palindromic-substring.py
+++ two-pointers/longest-palindromic-substring.py
@@ -1,30 +1,34 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        t = '#' + '#'.join(s) + '#'
-        n = len(t)
-        p = [0]*n
-        center = 0
-        right = 0
-        max_len = 0
-        max_center = 0
+        maxlen = 0
+        begin = 0
+        for i in range(len(s)):
+            l = i-1
+            r = i+1
 
-        for i in range(n):
-            mirror = 2*center - i
+            while l>=0 and r<len(s):
+                if s[l] == s[r]:
+                    l-=1
+                    r+=1
+                else:
+                    break
+            
+            if r-l-1>maxlen:
+                maxlen = r-l-1
+                begin = l+1
+            
+            l = i
+            r = i+1
 
-            if i<right:
-                p[i] = min(p[mirror], right-i)
+            while l>=0 and r<len(s):
+                if s[l] == s[r]:
+                    l-=1
+                    r+=1
+                else:
+                    break
             
-            while i-p[i]-1>=0 and i+p[i]+1<n and t[i-p[i]-1] == t[i+p[i]+1]:
-                p[i]+=1
-            
-            if i+p[i]>right:
-                right = i+p[i]
-                center = i
-            
-            if p[i]>max_len:
-                max_center = i
-                max_len = p[i]
+            if r-l-1>maxlen:
+                maxlen = r-l-1
+                begin = l+1
         
-        start = (max_center-max_len)//2
-
-        return s[start:start+max_len]
+        return s[begin:begin+maxlen]
