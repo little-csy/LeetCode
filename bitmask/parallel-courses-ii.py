02ffa4541937907a3e658b2250e49f181ba69e02
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-19 17:13:59 -0700

    LC-parallel-courses-ii [Runtime: 283 ms; Memory: 20.7 MB]

---
+++ bitmask/parallel-courses-ii.py
@@ -0,0 +1,41 @@
+from collections import deque
+from itertools import combinations
+class Solution:
+    def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
+        pre = [0] * n
+        q = deque()
+        visit = set()
+        for prev, nxt in relations:
+            pre[nxt-1] |= 1<<(prev-1)
+        
+        q.append((0,0))
+        visit.add(0)
+
+        while q:
+            mask, step = q.popleft()
+
+            if mask == (1<<n) -1:
+                return step
+            
+            can = []
+            for i in range(n):
+                if not(mask>>i & 1) and pre[i] == (pre[i] & mask):
+                    can.append(i)
+            
+            if len(can)<=k:
+                newmask = mask
+                for c in can:
+                    newmask |= (1<<c)
+                if newmask not in visit:
+                    visit.add(newmask)
+                    q.append((newmask, step+1))
+            else:
+                for comb in combinations(can,k):
+                    newmask = mask
+                    for c in comb:
+                        newmask |= (1<<c)
+                    if newmask not in visit:
+                        visit.add(newmask)
+                        q.append((newmask, step+1))
+        
+        return -1
