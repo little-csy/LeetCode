b24325efae7c7cc7bbb85b20c88622d7f3759e9d
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-19 20:23:42 -0700

    LC-maximum-number-of-events-that-can-be-attended [Runtime: 133 ms; Memory: 55.3 MB]

---
+++ array/maximum-number-of-events-that-can-be-attended.py
@@ -0,0 +1,27 @@
+import heapq
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        heap = []
+        events.sort()
+        n = len(events)
+        i = 0
+        day = 0
+        ans = 0
+
+        while i<n or heap:
+            if not heap:
+                day = events[i][0]
+            
+            while i<n and events[i][0]<=day:
+                heapq.heappush(heap, events[i][1])
+                i += 1
+            
+            while heap and heap[0]<day:
+                heapq.heappop(heap)
+            
+            if heap:
+                heapq.heappop(heap)
+                ans += 1
+                day += 1
+        
+        return ans
