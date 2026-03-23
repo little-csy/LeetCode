ac2ba03728dbe54fe2313d123420b0cfa9ec9dfb
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-22 20:28:33 -0700

    LC-parallel-courses-ii [Runtime: 299 ms; Memory: 20.6 MB]

--- graph/parallel-courses-ii.py
+++ graph/parallel-courses-ii.py
@@ -3,23 +3,22 @@ from itertools import combinations
 class Solution:
     def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
         pre = [0] * n
-        q = deque()
+        for prv, nxt in relations:
+            pre[nxt-1] |= 1<<(prv-1)
+        q =deque()
         visit = set()
-        for prev, nxt in relations:
-            pre[nxt-1] |= 1<<(prev-1)
-        
-        q.append((0,0))
+        q.append((0, 0))
         visit.add(0)
 
         while q:
             mask, step = q.popleft()
 
-            if mask == (1<<n) -1:
+            if mask == (1<<n)-1:
                 return step
-            
+
             can = []
             for i in range(n):
-                if not(mask>>i & 1) and pre[i] == (pre[i] & mask):
+                if (mask & (1<<i))==0 and pre[i] == (pre[i] & mask):
                     can.append(i)
             
             if len(can)<=k:
@@ -30,12 +29,11 @@ class Solution:
                     visit.add(newmask)
                     q.append((newmask, step+1))
             else:
-                for comb in combinations(can,k):
+                for comb in combinations(can, k):
                     newmask = mask
                     for c in comb:
                         newmask |= (1<<c)
                     if newmask not in visit:
                         visit.add(newmask)
                         q.append((newmask, step+1))
-        
         return -1
