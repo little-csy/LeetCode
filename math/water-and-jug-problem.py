08c3a465274cedbef91b42788c4d1e2c089f5f6d
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-03-31 20:09:17 -0700

    LC-water-and-jug-problem [Runtime: 1 ms; Memory: 19.5 MB]

--- math/water-and-jug-problem.py
+++ math/water-and-jug-problem.py
@@ -1,15 +1,12 @@
+import math
 class Solution:
     def canMeasureWater(self, x: int, y: int, target: int) -> bool:
         if target ==0 :
             return True
         if target>x+y:
             return False
-        def gcd(a,b):
-            while b:
-                a, b = b, a%b
-            return a
-        maxgcd = gcd(x,y)
-        if target%maxgcd == 0:
+
+        if target%math.gcd(x,y) == 0:
             return True
         else:
             return False
