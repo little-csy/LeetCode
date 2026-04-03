be53e5ee7d16b943b123b45ba0ccef3e485c0c4b
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-03 09:32:56 -0700

    LC-restore-ip-addresses [Runtime: 19 ms; Memory: 19.3 MB]

--- backtracking/restore-ip-addresses.py
+++ backtracking/restore-ip-addresses.py
@@ -3,21 +3,23 @@ class Solution:
         res = []
         path = []
 
-        def backtracking(s, start):
+        def dfs(s,start):
             if len(path) == 4:
-                if start >= len(s):
+                if start>=len(s):
                     res.append('.'.join(path))
-                return
             
             for i in range(start,len(s)):
                 seg = s[start:i+1]
-                if len(seg) > 1 and seg[0] == '0':
+                if len(seg)>1 and seg[0] == '0':
                     continue
-                if int(seg) > 255:
+                if int(seg)>255:
                     continue
                 path.append(seg)
-                backtracking(s, i+1)
+                dfs(s,i+1)
                 path.pop()
         
-        backtracking(s, 0)
+        
+
+        dfs(s, 0)
+
         return res
