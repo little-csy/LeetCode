b84e48248e949d7154f6f0a892b81b2b7ffbccf2
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-03 09:35:40 -0700

    LC-merge-k-sorted-lists [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/merge-k-sorted-lists.py
+++ divide-and-conquer/merge-k-sorted-lists.py
@@ -5,6 +5,13 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         if not lists:
             return None
         n = len(lists)
