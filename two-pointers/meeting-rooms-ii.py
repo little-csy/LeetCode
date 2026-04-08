941372a6b3d1b85d84b2610898c5cc96437d477e
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-08 14:50:59 -0700

    LC-meeting-rooms-ii [Runtime: N/A; Memory: N/A]

--- two-pointers/meeting-rooms-ii.py
+++ two-pointers/meeting-rooms-ii.py
@@ -1,13 +1,11 @@
 import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
         heap = []
-
+        intervals.sort()
         for s,e in intervals:
             if heap and s>heap[0]:
                 heapq.heappop(heap)
-            heapq.heappush(heap, e)
-        
+            heapq.heappush(heap,e)
         return len(heap)
         
