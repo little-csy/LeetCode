9c2a7c27baefd04b849deb84f9b085418ec5cfe5
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-08 14:35:37 -0700

    LC-number-of-islands [Runtime: 247 ms; Memory: 21.5 MB]

--- union-find/number-of-islands.py
+++ union-find/number-of-islands.py
@@ -2,22 +2,20 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m = len(grid)
         n = len(grid[0])
-        
-        def dfs(grid,i,j):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j] == "0":
-                return
-            grid[i][j] = "0"
-            dfs(grid, i+1, j)
-            dfs(grid, i-1, j)
-            dfs(grid, i, j+1)
-            dfs(grid, i, j-1)
 
+        def dfs(i,j):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j] == '0':
+                return
+            grid[i][j] = '0'
+            dfs(i+1,j)
+            dfs(i-1,j)
+            dfs(i,j-1)
+            dfs(i,j+1)
         
         cnt = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1":
+                if grid[i][j] == '1':
                     cnt += 1
-                    dfs(grid,i,j)
+                    dfs(i,j)
         return cnt
-        
