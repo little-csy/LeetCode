23a7533863896fbb6c8384eff1be49e415ade626
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-08 15:14:52 -0700

    LC-longest-increasing-subsequence [Runtime: 3 ms; Memory: 19.6 MB]

--- dynamic-programming/longest-increasing-subsequence.py
+++ dynamic-programming/longest-increasing-subsequence.py
@@ -3,10 +3,10 @@ class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         tail = []
         for x in nums:
-            idx = bisect.bisect_left(tail,x)
+            idx = bisect_left(tail,x)
             if idx == len(tail):
                 tail.append(x)
             else:
                 tail[idx] = x
-        return len(tail)
         
+        return len(tail)
