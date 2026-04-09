f338fa30e352bc7bcf57caebdc223a7361de1f5b
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-08 18:58:06 -0700

    LC-longest-mountain-in-array [Runtime: 15 ms; Memory: 20.3 MB]

--- dynamic-programming/longest-mountain-in-array.py
+++ dynamic-programming/longest-mountain-in-array.py
@@ -1,19 +1,15 @@
 class Solution:
     def longestMountain(self, arr: List[int]) -> int:
-        n = len(arr)
-        res = 0
-
-        for i in range(1,n-1):
-            if arr[i-1] < arr[i] > arr[i+1]:
+        length = 0
+        for i in range(1,len(arr)-1):
+            if arr[i-1]<arr[i]>arr[i+1]:
                 l = i-1
                 r = i+1
-
                 while l>0 and arr[l-1]<arr[l]:
                     l-=1
-                
-                while r<n-1 and arr[r+1]<arr[r]:
+                while r<len(arr)-1 and arr[r]>arr[r+1]:
                     r+=1
+                if r-l+1>length:
+                    length = r-l+1
+        return length
                 
-                if r-l+1>res:
-                    res = r-l+1
-        return res
