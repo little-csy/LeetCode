24b18f784a57fc718b3a8402ec91dd4ca1067aef
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-08 19:02:56 -0700

    LC-longest-palindromic-substring [Runtime: 171 ms; Memory: 19.2 MB]

--- string/longest-palindromic-substring.py
+++ string/longest-palindromic-substring.py
@@ -1,29 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        t = '#' + '#'.join(s) + '#'
-        p = [0] * len(t)
-        center = 0
-        right = 0
-        max_len = 0
-        max_center = 0
-
-        for i in range(len(t)):
-            mirror = 2*center - i
-
-            if i<right:
-                p[i] = min(p[mirror], right-i)
-            
-            while i-p[i]-1>=0 and i+p[i]+1<len(t) and t[i-p[i]-1] == t[i+p[i]+1]:
-                p[i] += 1
-            
-            if i+p[i]>right:
-                right = i+p[i]
-                center = i
-            
-            if p[i]>max_len:
-                max_len = p[i]
-                max_center = i
-        
-        start = (max_center-max_len)//2
-
-        return s[start:start+max_len]
+        n = len(s)
+        maxlen = 0
+        start = 0
+        for i in range(n):
+            l = i-1
+            r = i+1
+            while l>=0 and r<n and s[l]==s[r]:
+                l-=1
+                r+=1
+            if r-l-1>maxlen:
+                maxlen = r-l-1
+                start = l+1
+            l = i
+            r = i+1
+            while l>=0 and r<n and s[l]==s[r]:
+                l-=1
+                r+=1
+            if r-l-1>maxlen:
+                maxlen = r-l-1
+                start = l+1
+        return s[start:start+maxlen]
+                
