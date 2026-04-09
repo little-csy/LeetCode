227e8d3e1cca21d768a679e4000e7f919e9b3e59
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-08 17:00:39 -0700

    LC-subarray-sums-divisible-by-k [Runtime: 31 ms; Memory: 23.6 MB]

--- prefix-sum/subarray-sums-divisible-by-k.py
+++ prefix-sum/subarray-sums-divisible-by-k.py
@@ -1,6 +1,7 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        cnt = {0:1}
+        cnt = defaultdict(int)
+        cnt[0]=1
         pre = 0
         res = 0
 
@@ -9,5 +10,5 @@ class Solution:
             mod = pre%k
             if mod in cnt:
                 res += cnt[mod]
-            cnt[mod] = cnt.get(mod,0)+1
+            cnt[mod] += 1
         return res
