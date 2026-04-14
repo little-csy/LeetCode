982a36f9428c81bed758dac7fcff6d1b07615ba7
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-13 20:25:10 -0700

    LC-design-in-memory-file-system [Runtime: 3 ms; Memory: 19.6 MB]

--- sorting/design-in-memory-file-system.py
+++ sorting/design-in-memory-file-system.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self):
         self.child = {}
-        self.isfile = False
+        self.isFile = False
         self.content = ""
 
 class FileSystem:
@@ -11,39 +11,36 @@ class FileSystem:
         
     def ls(self, path: str) -> List[str]:
         cur = self.root
-        if path != '/':
-            for part in path.split("/")[1:]:
+        if path!='/':
+            for part in path.split('/')[1:]:
                 cur = cur.child[part]
-        
-        if cur.isfile == True:
-            return [path.split("/")[-1]]
+        if cur.isFile:
+            return [path.split('/')[-1]]
         else:
             return sorted(cur.child.keys())
-
+    
     def mkdir(self, path: str) -> None:
         cur = self.root
-        for part in path.split("/")[1:]:
+        for part in path.split('/')[1:]:
             if part not in cur.child:
                 cur.child[part] = Node()
             cur = cur.child[part]
         
     def addContentToFile(self, filePath: str, content: str) -> None:
         cur = self.root
-        for part in filePath.split("/")[1:]:
+        for part in filePath.split('/')[1:]:
             if part not in cur.child:
                 cur.child[part] = Node()
             cur = cur.child[part]
-        
-        cur.isfile = True
+        cur.isFile = True
         cur.content += content
-
+        
     def readContentFromFile(self, filePath: str) -> str:
         cur = self.root
-        for part in filePath.split("/")[1:]:
+        for part in filePath.split('/')[1:]:
             cur = cur.child[part]
-        
         return cur.content
-        
+
 # Your FileSystem object will be instantiated and called as such:
 # obj = FileSystem()
 # param_1 = obj.ls(path)
