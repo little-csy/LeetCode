906a477607dbf1498b3799f76493cda04f5bab99
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-13 21:09:44 -0700

    LC-word-search [Runtime: 3323 ms; Memory: 19.5 MB]

--- depth-first-search/word-search.py
+++ depth-first-search/word-search.py
@@ -1,20 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        def dfs(i:int, j:int, index:int) -> bool:
+        m = len(board)
+        n = len(board[0])
+
+        def dfs(i,j,index):
             if len(word) == index:
                 return True
-            if i<0 or i>= len(board) or j<0 or j>=len(board[0]) or word[index] != board[i][j]:
+            if i<0 or i>=m or j<0 or j>=n or board[i][j]!=word[index]:
                 return False
-            temp = board[i][j]
+            tmp = board[i][j]
             board[i][j] = '#'
-            found = dfs(i+1,j,index+1) or dfs(i-1,j,index+1) or dfs(i,j-1,index+1) or dfs(i,j+1,index+1)
-            board[i][j] = temp
+            found = dfs(i+1,j,index+1) or dfs(i-1,j,index+1) or dfs(i,j+1,index+1) or dfs(i,j-1,index+1)
+            board[i][j] = tmp
             return found
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
+
+
+
+        for i in range(m):
+            for j in range(n):
                 if dfs(i,j,0):
                     return True
         return False
-        
