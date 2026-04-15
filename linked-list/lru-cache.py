dc1fb653854f8a88ab572eaa646c45d4dfaa4dcb
Siyu Chen <73229305+little-csy@users.noreply.github.com>
2026-04-14 19:08:28 -0700

    LC-lru-cache [Runtime: 151 ms; Memory: 76.9 MB]

--- linked-list/lru-cache.py
+++ linked-list/lru-cache.py
@@ -1,49 +1,49 @@
 class Node:
-    def __init__(self,key,val):
+    def __init__(self,key,value):
         self.key = key
-        self.val = val
+        self.value = value
         self.next = None
         self.pre = None
 
 class LRUCache:
 
-    def __init__(self, capacity: int):
+    def __init__(self,capacity):
         self.head = Node(0,0)
         self.tail = Node(0,0)
         self.head.next = self.tail
         self.tail.pre = self.head
         self.hp = {}
         self.capacity = capacity
-    
-    def addn(self, node):
+
+    def addn(self,node):
         nxt = self.head.next
         self.head.next = node
         node.pre = self.head
         node.next = nxt
         nxt.pre = node
-    
-    def removen(self, node):
-        p = node.pre
-        n = node.next
-        p.next = n
-        n.pre = p
-        
-    def get(self, key: int) -> int:
+
+    def removen(self,node):
+        front = node.pre
+        back = node.next
+        front.next = back
+        back.pre = front
+
+    def get(self,key):
         if key in self.hp:
             node = self.hp[key]
             self.removen(node)
             self.addn(node)
-            return node.val
+            return node.value
         return -1
         
-    def put(self, key: int, value: int) -> None:
+    def put(self,key,value):
         if key in self.hp:
             node = self.hp[key]
             self.removen(node)
             del self.hp[key]
-        new = Node(key,value)
-        self.addn(new)
-        self.hp[key] = new
+        newnode = Node(key,value)
+        self.addn(newnode)
+        self.hp[key] = newnode
         if len(self.hp)>self.capacity:
             last = self.tail.pre
             self.removen(last)
